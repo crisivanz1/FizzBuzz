a740f35ef5feaaac6c4c5257f078a4553ed747d2
crisivanz1 <79119785+crisivanz1@users.noreply.github.com>
2022-05-08 01:40:21 -0400

    Add files via upload

---
+++ FizzBuzz/FBMenu.py
@@ -0,0 +1,22 @@
+from FBComputer import Default
+from FBOnePlayer import OnePlayer
+from FBTwoPlayer import TwoPlayer
+from FBPlayerVsCPU import VsCPU
+
+menuOptions = ["1", "2", "3", "4"]
+print("Welcome to the FizzBuzz program!")
+
+while True:
+    userInput = str(input("Choose a menu option: "))
+    if userInput not in menuOptions:
+        print("bruh, choose again")
+    else:
+        if userInput == menuOptions[0]:
+            Default()
+        elif userInput == menuOptions[1]:
+            OnePlayer()
+        elif userInput == menuOptions[2]:
+            VsCPU()
+        elif userInput == menuOptions[3]:
+            TwoPlayer()
+        break
